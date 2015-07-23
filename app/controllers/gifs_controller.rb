e90bd93ba0931d1225cb9c31772895755b8325b4
apradhn <abhijit.pradhan6@gmail.com>
2015-07-23 17:26:47 -0400

    added action and view for search; built site layout

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -3,5 +3,9 @@ class GifsController < ApplicationController
   def index
     @gifs = Gif.all
   end
-  
+
+  def search
+    @gif = Gif.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'gifs#index'
+  root to: 'gifs#index'
+
+  get 'search' => 'gifs#search', as: :search
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
